4d959833a05e6e79eaae3e914d1570c5a53edb0c
Duy Trieu <trieuvuduy@gmail.com>
2018-11-09 23:20:10 -0500

    add playoractorAPI

---
+++ doc/api/engine/frontend/ActorUI.java
@@ -0,0 +1,8 @@
+package engine.frontend;
+
+public Interface ActorUI {
+    public void setX();
+    public void setY();
+    public void setAnimation(Animation animation);
+    public Dimension getBoundaryBox();
+}
