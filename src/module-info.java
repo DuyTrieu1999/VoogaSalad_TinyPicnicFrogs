3b310a5b4f906fd9b8e291ac74e194ee561eba2f
Duy Trieu <trieuvuduy@gmail.com>
2018-11-09 21:52:27 -0500

    UI testing

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module voogasalad_tinypicnicfrogs {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.web;
+    requires java.xml;
+
+    exports game_engine_UI;
+}
