aa424a25b180d8ec9e53640d2d79f5dca2b0d1d0
Duy Trieu <trieuvuduy@gmail.com>
2018-11-11 16:44:08 -0500

    add init code for stateView and Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,11 @@
 package Controller;
 
-public class Controller {
+import game_engine_UI.StateView;
 
+public class Controller {
+    private StateView myView;
+
+    public Controller (StateView view) {
+        this.myView = view;
+    }
 }
--- src/game_engine_UI/StateView.java
+++ src/game_engine_UI/StateView.java
@@ -1,7 +1,21 @@
 package game_engine_UI;
 
+import Controller.Controller;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 public class StateView {
+    private Controller myController;
+    private StackPane myPane;
+    private static Pane myBackGround;
+
     public void setGameWorld() {
+        myController = new Controller(this);
+        myBackGround = new Pane();
+        myPane = new StackPane();
+        setUpScene();
+    }
+    private void setUpScene () {
 
     }
 }
