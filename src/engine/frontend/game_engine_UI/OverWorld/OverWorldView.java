dbd1bf1bb67f248c79661ef70f01fe0df2726874
Duy Trieu <trieuvuduy@gmail.com>
2018-11-13 21:08:00 -0500

    update state view

--- src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
+++ src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
@@ -1,4 +1,12 @@
 package engine.frontend.game_engine_UI.OverWorld;
 
+import javafx.scene.Scene;
+
 public class OverWorldView implements OverWorldViewAPI {
+    private Scene myWorldScene;
+
+    public OverWorldView () {
+
+    }
+
 }
