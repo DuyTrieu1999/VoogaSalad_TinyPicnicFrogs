4488a9a01506b758a1311a21a67c1bc174b5c148
Duy Trieu <trieuvuduy@gmail.com>
2018-11-13 22:17:47 -0500

    update Controller

--- src/engine/controller/Controller.java
+++ src/engine/controller/Controller.java
@@ -1,9 +1,11 @@
 package engine.controller;
 
+import engine.backend.ServiceLocator;
 import engine.frontend.game_engine_UI.StateView;
 
 public class Controller {
     private StateView myView;
+    private ServiceLocator myLocator;
 
     public Controller (StateView view) {
         this.myView = view;
