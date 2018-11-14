8c5d1eb0aa89c3fdfc33f3f59a9a31a1a8d83ddf
Duy Trieu <trieuvuduy@gmail.com>
2018-11-14 15:03:02 -0500

    update worldview

--- src/engine/frontend/game_engine_UI/OverWorld/Camera.java
+++ src/engine/frontend/game_engine_UI/OverWorld/Camera.java
@@ -1,8 +1,12 @@
 package engine.frontend.game_engine_UI.OverWorld;
 
+import engine.backend.PlayerActor;
 import javafx.geometry.Point2D;
 
 public class Camera {
+    public Camera(PlayerActor player) {
+
+    }
     public void getPlayerLoc(Point2D loc) {
 
     }
--- src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
+++ src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
@@ -3,7 +3,8 @@ package engine.frontend.game_engine_UI.OverWorld;
 import engine.backend.Actor;
 import engine.backend.PlayerActor;
 import engine.frontend.Animation;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 
 import java.util.Collection;
 
@@ -17,7 +18,31 @@ public class OverWorldView implements OverWorldViewAPI {
     private PlayerActor myPlayer;
 
     public OverWorldView () {
+        this.setUpDisplay();
+    }
+    public void updateWorldView () {
+        clearOverWorld();
+        this.addActors();
+    }
+    private void addActors () {
 
     }
-
+    private void clearOverWorld () {
+        this.myAnimations.clear();
+        displayPane.getChildren().clear();
+    }
+    private void setUpDisplay () {
+        displayPane = new BorderPane();
+        displayPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        HBox.setHgrow(displayPane, Priority.ALWAYS);
+        VBox.setVgrow(displayPane, Priority.ALWAYS);
+        clipBound(displayPane);
+        this.updateWorldView();
+    }
+    private void clipBound(Pane pane) {
+        Rectangle clipBoundaries = new Rectangle();
+        clipBoundaries.widthProperty().bind(pane.widthProperty());
+        clipBoundaries.heightProperty().bind(pane.heightProperty());
+        pane.setClip(clipBoundaries);
+    }
 }
