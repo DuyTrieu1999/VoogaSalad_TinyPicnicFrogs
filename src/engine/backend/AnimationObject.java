0b5a0fc17f70225498d0a94a2c649b74679d1f05
Duy Trieu <trieuvuduy@gmail.com>
2018-11-15 15:15:16 -0500

    add animation object

---
+++ src/engine/backend/AnimationObject.java
@@ -0,0 +1,12 @@
+package engine.backend;
+
+import javafx.scene.image.ImageView;
+
+public class AnimationObject {
+    private String animationName;
+    ImageView animationView;
+
+    public AnimationObject (String name) {
+        this.animationName = name;
+    }
+}
