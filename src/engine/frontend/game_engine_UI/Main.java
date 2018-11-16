6fe39eb083ff49133ecb722a21096954367fdd01
Duy Trieu <trieuvuduy@gmail.com>
2018-11-15 19:11:35 -0500

    implementing worldView

--- src/engine/frontend/game_engine_UI/Main.java
+++ src/engine/frontend/game_engine_UI/Main.java
@@ -1,10 +1,16 @@
 package engine.frontend.game_engine_UI;
 
+import engine.frontend.game_engine_UI.AnimationProcesser.AnimationProcesser;
+import javafx.animation.Animation;
 import javafx.application.Application;
-
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
-
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
     private static final Image IMAGE = new Image("https://kwiksher.com/wp-content/uploads/2012/09/runningcat.png");
@@ -21,6 +27,24 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Testing");
 
+        final ImageView imageView = new ImageView(IMAGE);
+        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+
+        final Animation animation = new AnimationProcesser(
+                imageView,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+        HBox box = new HBox();
+        box.getChildren().add(imageView);
+
+        primaryStage.setScene(new Scene(new Group(box)));
+        primaryStage.show();
     }
 }
--- src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
+++ src/engine/frontend/game_engine_UI/OverWorld/OverWorldView.java
@@ -4,13 +4,13 @@ import engine.backend.Actor;
 import engine.backend.AnimationObject;
 import engine.backend.PlayerActor;
 import engine.controller.Controller;
+import engine.frontend.game_engine_UI.WorldView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
 
 import java.util.*;
 
-public class OverWorldView implements OverWorldViewAPI {
+public class OverWorldView extends WorldView implements OverWorldViewAPI {
     private Collection<Actor> myActors;
     private Collection<AnimationObject> myAnimations;
 
@@ -20,48 +20,30 @@ public class OverWorldView implements OverWorldViewAPI {
     private PlayerActor myPlayer;
 
     public OverWorldView (PlayerActor player, Controller controller) {
-        myActors = controller.getActor();
-        myAnimations = controller.getAnimation();
-        myPlayer = player;
+        super(player, controller);
         myCamera = new Camera(myPlayer);
-        this.setUpDisplay();
     }
-    public void updateWorldView () {
-        clearOverWorld();
-        this.addActors();
-        this.setViewByZ();
-    }
-    private void addActors () {
-        for (Actor actor: myActors) {
+    public void setUpWorld () {
 
-        }
     }
-    private void moveCamera () {
+    @Override
+    public void updateView () {
+
+    }
+    public void setCamera(Camera newCamera) {
+        this.myCamera = newCamera;
+    }
+    public Camera getCamera() {
+        return this.myCamera;
+    }
+    public Pane getView () {
+        return displayPane;
+    }
+    public void moveCamera () {
         for (AnimationObject animationObject: myAnimations) {
             ImageView animationView = animationObject.getAnimationView();
-            animationView.setTranslateX(myCamera.getTranslateX());
-            animationView.setTranslateY(myCamera.getTranslateY());
+            animationView.setTranslateX(- myCamera.getTranslateX());
+            animationView.setTranslateY(- myCamera.getTranslateY());
         }
-    }
-    private void clearOverWorld () {
-        this.myAnimations.clear();
-        displayPane.getChildren().clear();
-    }
-    private void setUpDisplay () {
-        displayPane = new BorderPane();
-        displayPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        HBox.setHgrow(displayPane, Priority.ALWAYS);
-        VBox.setVgrow(displayPane, Priority.ALWAYS);
-        clipBound(displayPane);
-        this.updateWorldView();
-    }
-    private void clipBound(Pane pane) {
-        Rectangle clipBoundaries = new Rectangle();
-        clipBoundaries.widthProperty().bind(pane.widthProperty());
-        clipBoundaries.heightProperty().bind(pane.heightProperty());
-        pane.setClip(clipBoundaries);
-    }
-    private void setViewByZ() {
-
     }
 }
