b41ac900481c0fb2849a768ca1659f334d304069
Max Bartlett <mmb70@duke.edu>
2018-11-19 20:53:35 -0500

    Actormanager max

--- src/engine/backend/ActorManager.java
+++ src/engine/backend/ActorManager.java
@@ -1,5 +1,6 @@
 package engine.backend;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ActorManager {
@@ -10,9 +11,44 @@ public class ActorManager {
 
     ActorManager(List<Actor> actorList){
         allActors = actorList;
+        inactiveActors = allActors;
+        activeActors = new ArrayList<>();
     }
 
     public PlayerActor getPlayerActor(){
         return myPlayerActor;
     }
+
+    public List<Actor> getActiveActors() { return activeActors;}
+
+    public List<Actor> getInactiveActors() { return inactiveActors;}
+
+    /**
+     * changes an actor's state
+     * @param removeList
+     * @param addList
+     * @param actor
+     */
+    private void changeActorState(List<Actor> removeList, List<Actor> addList, Actor actor) {
+        removeList.remove(actor);
+        addList.add(actor);
+    }
+
+    /**
+     * activates a given actor
+     * @param actor
+     */
+    public void activate(Actor actor) {
+        changeActorState(inactiveActors, activeActors, actor);
+    }
+
+    /**
+     * inactivates a given actor
+     * @param actor
+     */
+    public void inactivate(Actor actor) {
+        changeActorState(activeActors, inactiveActors, actor);
+    }
+
+
 }
