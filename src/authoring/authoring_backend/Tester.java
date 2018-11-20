64cd74ea52092b273b2011c444568fc04f088d8d
Michael Glushakov <mg367@duke.edu>
2018-11-20 10:34:32 -0500

    reqrote tests to use files, tested multiple prototupe/actor storage

--- src/authoring/authoring_backend/Tester.java
+++ src/authoring/authoring_backend/Tester.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.File;
 import java.io.FileReader;
 
 public class Tester {
@@ -22,49 +23,52 @@ public class Tester {
     }
         private static JSONObject loadJSON(){
             JSONParser parser = new JSONParser();
+
             try{
-                JSONObject obj=(JSONObject) parser.parse("{\n" +
-                        "  \"name\":\"charizard\",\n" +
-                        "  \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard1.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard2.png\"}],\n" +
-                        "  \"stats\":[{\"key\":\"health\",\"value\":50},{\"key\":\"damage\",\"value\":5}\n" +
-                        "  ],\n" +
-                        "  \"Interactions\": [{\n" +
-                        "    \"name\":\"fight1\",\n" +
-                        "    \"type\":\"fight\",\n" +
-                        "    \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard3.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard4.png\"}],\n" +
-                        "    \"Messages\":[\n" +
-                        "      {\"key\":\"prototypeVictory\",\"messageKey\":\"onVictory\"},\n" +
-                        "      {\"key\":\"prototypeDefeat\",\"messageKey\":\"onDefeat\"}\n" +
-                        "    ],\n" +
-                        "\n" +
-                        "    \"moves\":[\n" +
-                        "      {\n" +
-                        "        \"name\":\"basic attack\",\n" +
-                        "        \"targetStat\": \"HP\",\n" +
-                        "        \"targetActorNumber\":1,\n" +
-                        "        \"targetActorType\":\"enemy\",\n" +
-                        "        \"targetValue\":10,\n" +
-                        "        \"targetType\":\"constant\"\n" +
-                        "      },{\n" +
-                        "        \"name\":\"basic regen\",\n" +
-                        "        \"targetStat\": \"HP\",\n" +
-                        "        \"targetActorNumber\":1,\n" +
-                        "        \"targetActorType\":\"friend\",\n" +
-                        "        \"targetValue\":10,\n" +
-                        "        \"targetType\":\"percent\"\n" +
-                        "      },{\n" +
-                        "        \"name\":\"special attack\",\n" +
-                        "        \"targetStat\": \"HP\",\n" +
-                        "        \"targetActorNumber\":3,\n" +
-                        "        \"targetActorType\":\"enemy\",\n" +
-                        "        \"targetValue\":10,\n" +
-                        "        \"targetType\":\"percent\"\n" +
-                        "      }\n" +
-                        "    ]\n" +
-                        "\n" +
-                        "  }\n" +
-                        "  ]\n" +
-                        "}");
+                JSONObject obj = (JSONObject) parser.parse(new File("./resources/TestPrototype.json").toString());
+                System.out.println("JSON:"+obj.toJSONString());
+//                JSONObject obj=(JSONObject) parser.parse("{\n" +
+//                        "  \"name\":\"charizard\",\n" +
+//                        "  \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard1.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard2.png\"}],\n" +
+//                        "  \"stats\":[{\"key\":\"health\",\"value\":50},{\"key\":\"damage\",\"value\":5}\n" +
+//                        "  ],\n" +
+//                        "  \"Interactions\": [{\n" +
+//                        "    \"name\":\"fight1\",\n" +
+//                        "    \"type\":\"fight\",\n" +
+//                        "    \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard3.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard4.png\"}],\n" +
+//                        "    \"Messages\":[\n" +
+//                        "      {\"key\":\"prototypeVictory\",\"messageKey\":\"onVictory\"},\n" +
+//                        "      {\"key\":\"prototypeDefeat\",\"messageKey\":\"onDefeat\"}\n" +
+//                        "    ],\n" +
+//                        "\n" +
+//                        "    \"moves\":[\n" +
+//                        "      {\n" +
+//                        "        \"name\":\"basic attack\",\n" +
+//                        "        \"targetStat\": \"HP\",\n" +
+//                        "        \"targetActorNumber\":1,\n" +
+//                        "        \"targetActorType\":\"enemy\",\n" +
+//                        "        \"targetValue\":10,\n" +
+//                        "        \"targetType\":\"constant\"\n" +
+//                        "      },{\n" +
+//                        "        \"name\":\"basic regen\",\n" +
+//                        "        \"targetStat\": \"HP\",\n" +
+//                        "        \"targetActorNumber\":1,\n" +
+//                        "        \"targetActorType\":\"friend\",\n" +
+//                        "        \"targetValue\":10,\n" +
+//                        "        \"targetType\":\"percent\"\n" +
+//                        "      },{\n" +
+//                        "        \"name\":\"special attack\",\n" +
+//                        "        \"targetStat\": \"HP\",\n" +
+//                        "        \"targetActorNumber\":3,\n" +
+//                        "        \"targetActorType\":\"enemy\",\n" +
+//                        "        \"targetValue\":10,\n" +
+//                        "        \"targetType\":\"percent\"\n" +
+//                        "      }\n" +
+//                        "    ]\n" +
+//                        "\n" +
+//                        "  }\n" +
+//                        "  ]\n" +
+//                        "}");
                 return obj;
             }catch (ParseException e){
                 e.printStackTrace();
--- test/authoring/backend/GameManagetTester.java
+++ test/authoring/backend/GameManagetTester.java
@@ -7,6 +7,10 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -48,67 +52,25 @@ public class GameManagetTester {
         manager.createMessage("onVictory","Player won");
         manager.createMessage("onDefeat","Game over");
         manager.saveGame("./resources/","./resources/authoring/");
-        JSONObject data = loadJSON();
+        JSONObject data = loadJSON("./resources/TestPrototype.json");
+        JSONObject data2 =loadJSON("./resources/TestPrototype2.json");
         if(data!=null){
             manager.createActorPrototype(data);
+            manager.createActorPrototype(data2);
             manager.createActor("charizard",0,0,0,0,0);
+            manager.createActor("pikachu",0,0,0,0,0);
             manager.saveGame("./resources/","./resources/authoring/");
         }
     }
-    private  JSONObject loadJSON(){
+    private  JSONObject loadJSON(String path){
         JSONParser parser = new JSONParser();
         try{
-            JSONObject obj=(JSONObject) parser.parse("{\n" +
-                    "  \"name\":\"charizard\",\n" +
-                    "  \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard1.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard2.png\"}],\n" +
-                    "  \"stats\":[{\"key\":\"health\",\"value\":50},{\"key\":\"damage\",\"value\":5},\n" +
-                    "\n" +
-                    "  ],\n" +
-                    "  \"isPlayer\":false,\n" +
-                    "  \"Interactions\": [{\n" +
-                    "    \"name\":\"fight1\",\n" +
-                    "    \"type\":\"fight\",\n" +
-                    "    \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard3.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard4.png\"}],\n" +
-                    "    \"Messages\":[\n" +
-                    "      {\"key\":\"prototypeVictory\",\"messageKey\":\"onVictory\"},\n" +
-                    "      {\"key\":\"prototypeDefeat\",\"messageKey\":\"onDefeat\"}\n" +
-                    "    ],\n" +
-                    "\n" +
-                    "    \"moves\":[\n" +
-                    "      {\n" +
-                    "        \"name\":\"basic attack\",\n" +
-                    "        \"targetStat\": \"HP\",\n" +
-                    "        \"targetActorNumber\":1,\n" +
-                    "        \"targetActorType\":\"enemy\",\n" +
-                    "        \"targetValue\":10,\n" +
-                    "        \"targetType\":\"constant\",\n" +
-                    "        \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard5.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard6.png\"}]\n" +
-                    "      },{\n" +
-                    "        \"name\":\"basic regen\",\n" +
-                    "        \"targetStat\": \"HP\",\n" +
-                    "        \"targetActorNumber\":1,\n" +
-                    "        \"targetActorType\":\"friend\",\n" +
-                    "        \"targetValue\":10,\n" +
-                    "        \"targetType\":\"percent\",\n" +
-                    "        \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard5.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard6.png\"}]\n" +
-                    "      },{\n" +
-                    "        \"name\":\"special attack\",\n" +
-                    "        \"targetStat\": \"HP\",\n" +
-                    "        \"targetActorNumber\":3,\n" +
-                    "        \"targetActorType\":\"enemy\",\n" +
-                    "        \"targetValue\":10,\n" +
-                    "        \"targetType\":\"percent\",\n" +
-                    "        \"animations\":[{\"key\":\"default\",\"path\":\"/resource/charizard5.png\"},{\"key\":\"special\",\"path\":\"/resource/charizard6.png\"}]\n" +
-                    "      }\n" +
-                    "    ]\n" +
-                    "\n" +
-                    "  }\n" +
-                    "  ]\n" +
-                    "}");
+
+            JSONObject obj=(JSONObject) parser.parse(new FileReader(path));
             return obj;
         }catch (ParseException e){
             e.printStackTrace();
-        }
+        }catch (IOException e){e.printStackTrace();}
 
 
         return null;
