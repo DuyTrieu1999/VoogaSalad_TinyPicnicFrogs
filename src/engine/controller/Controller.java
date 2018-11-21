28a733d39746f2addcee6c3cb2601ad3b2360afa
Duy Trieu <trieuvuduy@gmail.com>
2018-11-21 02:54:02 -0600

    refactor controller

--- src/engine/controller/Controller.java
+++ src/engine/controller/Controller.java
@@ -8,6 +8,8 @@ import engine.frontend.game_engine_UI.StateView;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Controller {
     private StateView myView;
@@ -16,12 +18,15 @@ public class Controller {
         this.myView = view;
     }
 
-    public Collection<AnimationObject> getAnimation () {
-        return ServiceLocator.getActorManager().getAnimationObjects();
-    }
-    public PlayerActor getPlayer () {
-        return ServiceLocator.getActorManager().getPlayerActor();
-    }
-    public List<Command> getActiveCommands () { return myView.getActiveCommand(); }
-    public void setAllCommand(List<Command> commands) { setAllCommand(commands); }
+    private Supplier<Collection<AnimationObject>> animationObjectSupplier = () -> ServiceLocator.getActorManager().getAnimationObjects();
+    public Collection<AnimationObject> getAnimation () { return animationObjectSupplier.get(); }
+
+    private Supplier<PlayerActor> playerActorSupplier = () -> ServiceLocator.getActorManager().getPlayerActor();
+    public PlayerActor getPlayer () { return playerActorSupplier.get(); }
+
+    private Supplier<List<Command>> activeCommandSupplier = () -> myView.getActiveCommand();
+    public List<Command> getActiveCommands () { return activeCommandSupplier.get(); }
+
+    private Consumer<List<Command>> allCommandConsumer = e -> myView.setAllCommand(e);
+    public void setAllCommand(List<Command> commands) { allCommandConsumer.accept(commands); }
 }
