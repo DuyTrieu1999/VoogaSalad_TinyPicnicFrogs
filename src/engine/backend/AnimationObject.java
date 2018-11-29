c7b93c3781bf4e5c02224cbf2e9724781b43be5c
Michael Glushakov <mg367@duke.edu>
2018-11-28 19:58:53 -0500

    testing

--- src/engine/backend/AnimationObject.java
+++ src/engine/backend/AnimationObject.java
@@ -1,5 +1,6 @@
 package engine.backend;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class AnimationObject {
@@ -11,10 +12,11 @@ public class AnimationObject {
     public AnimationObject (String name,String path) {
         this.animationName = name;
         animationPath=path;
+        animationView= new ImageView(new Image(path));
     }
     public ImageView getAnimationView () {
         return this.animationView;
     }
-    public String getName(){return animationName;}
+    public String getName(){return animationName+": "+animationPath;}
     public Coordinate getCoordinate(){return coordinate;}
 }
