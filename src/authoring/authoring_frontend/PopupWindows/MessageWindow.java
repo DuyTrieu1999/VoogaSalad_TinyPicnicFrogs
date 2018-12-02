3d584940bab57a96614c931d3e0eec24113fedbe
bzk2 <bzk2@duke.edu>
2018-12-02 15:00:57 -0500

    starting to refactor popupwindow hierarchy

--- src/authoring/authoring_frontend/PopupWindows/MessageWindow.java
+++ src/authoring/authoring_frontend/PopupWindows/MessageWindow.java
@@ -1,48 +1,36 @@
-package authoring.authoring_frontend;
+package authoring.authoring_frontend.PopupWindows;
 
 import authoring.authoring_backend.GameManager;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import authoring.authoring_frontend.MessageForm;
 
-import java.util.ResourceBundle;
-
-public class MessageWindow {
-    public static final String DEFAULT_RESOURCE = "English";
-    public static int SIZE = 500;
-    private ResourceBundle myResources;
-    private Scene myWindow;
-    private Group myRoot;
+/**
+ * Message Window
+ *
+ * @author janiceliu
+ * @author brookekeene
+ */
+public class MessageWindow extends PopupWindow{
+    private int size;
     private MessageForm myMessageForm;
-    private GameManager myManager;
 
-    public MessageWindow(GameManager manager) {
-        myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE);
-        myRoot = new Group();
-        myWindow = new Scene(myRoot, SIZE, SIZE);
-        myManager = manager;
+    /**
+     * Constructor
+     */
+    public MessageWindow(GameManager manager, int n) {
+        super(manager, n);
+        size = n;
 
-        this.display();
+        this.display(myResources.getString("CreateMessage"));
         this.addContent();
     }
 
-    private void display() {
-        Stage window = new Stage(); //TODO: create preference width and height
 
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle(myResources.getString("messages"));
-
-        window.setScene(myWindow);
-        window.show();
-    }
-
-    private void addContent() {
-        ScrollPane mySP = new ScrollPane();
+    /**
+     * creates UI elements needed to get and save new
+     * message information
+     */
+    public void addContent() {
         myMessageForm = new MessageForm(myManager);
-        mySP.setContent(myMessageForm);
-        mySP.setMaxSize(SIZE,SIZE);
-        myRoot.getChildren().add(mySP);
+        myRoot.getChildren().add(myMessageForm);
     }
 }
--- src/authoring/authoring_frontend/PopupWindows/PrototypeWindow.java
+++ src/authoring/authoring_frontend/PopupWindows/PrototypeWindow.java
@@ -1,6 +1,7 @@
-package authoring.authoring_frontend;
+package authoring.authoring_frontend.PopupWindows;
 
 import authoring.authoring_backend.GameManager;
+import authoring.authoring_frontend.PrototypeForm;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
