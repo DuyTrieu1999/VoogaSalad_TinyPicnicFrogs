5084c8e61551bee64d54919a433a15d0809391ec
mmb70 <mmb70@duke.edu>
2018-12-07 23:55:13 -0500

    Fixed AnimationObject

--- src/engine/backend/AnimationObject.java
+++ src/engine/backend/AnimationObject.java
@@ -5,40 +5,86 @@ import javafx.scene.image.ImageView;
 
 /**
  * Represents an AnimationObject
- * @author
+ *
+ * @author Michael Glushakov (mg367)
+ * @author Max Bartlett (mmb70)
  */
 
 public class AnimationObject {
-    private String animationName;
-    private String animationPath;
-    private ImageView animationView;
-    private Coordinate coordinate;
-    private int spriteRows;
-    private int spiteCols;
-    public AnimationObject (String name,String path,int sriteR,int spriteC) {
-        this.animationName = name;
-        animationPath=path;
-        animationView= new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(path)));
-        spriteRows=sriteR;
-        spiteCols=spriteC;
-    }
+	private String animationName;
+	private String animationPath;
+	private ImageView animationView;
+	private Coordinate coordinate;
+	private int spriteRows;
+	private int spiteCols;
 
-    public AnimationObject (String name,String path,Coordinate coordinateP,int sriteR,int spriteC) {
-        this.animationName = name;
-        animationPath=path;
-        animationView= new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(animationPath)));
-        coordinate=coordinateP;
-        spriteRows=sriteR;
-        spiteCols=spriteC;
-    }
-    public ImageView getAnimationView () {
-        return this.animationView;
-    }
-    public String getName(){return animationName+": "+animationPath;}
-    public Coordinate getCoordinate(){return coordinate;}
-    public void setImage(){
-        animationView= new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(animationPath)));
-    }
-    public int getSpriteRows(){return spriteRows;}
-    public int getSpiteCols(){return spiteCols;}
+	/**
+	 * Constructor for AnimationObject
+	 * @param name name
+	 * @param path file path
+	 * @param spriteR sprite row
+	 * @param spriteC sprite column
+	 */
+	public AnimationObject(String name, String path, int spriteR, int spriteC) {
+		this.animationName = name;
+		animationPath = path;
+		setImage();
+		spriteRows = spriteR;
+		spiteCols = spriteC;
+	}
+
+	/**
+	 * Constructor for AnimationObject
+	 * @param name name
+	 * @param path file path
+	 * @param spriteR sprite row
+	 * @param spriteC sprite column
+	 * @param coordinateP coordinate
+	 */
+	public AnimationObject(String name, String path, int spriteR, int spriteC, Coordinate coordinateP) {
+		this(name, path, spriteR, spriteC);
+		coordinate = coordinateP;
+	}
+
+	/**
+	 * @return animationView
+	 */
+	public ImageView getAnimationView() {
+		return this.animationView;
+	}
+
+	/**
+	 * @return animationName: animationPath
+	 */
+	public String getName() {
+		return animationName + ": " + animationPath;
+	}
+
+	/**
+	 * @return coordinate
+	 */
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+
+	/**
+	 * sets image
+	 */
+	public void setImage() {
+		animationView = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(animationPath)));
+	}
+
+	/**
+	 * @return spriteRows
+	 */
+	public int getSpriteRows() {
+		return spriteRows;
+	}
+
+	/**
+	 * @return spriteCols
+	 */
+	public int getSpiteCols() {
+		return spiteCols;
+	}
 }
