f33e15daf07efcc02a34525db239700ee174d1c1
Christopher Lin <cl349@duke.edu>
2018-12-09 15:45:48 -0500

    change overlaps

--- src/engine/backend/GameWorld.java
+++ src/engine/backend/GameWorld.java
@@ -84,15 +84,18 @@ public class GameWorld {
         var collisionList = new ArrayList<Actor>();
         var playerActor = ServiceLocator.getActorManager().getPlayerActor();
         for(Actor a : actorList){
-            if(overlaps(playerActor, a)){
+            int[] overlap = overlaps(playerActor, a);
+            if(overlap[0] != 0 && overlap[1] != 0){
                 collisionList.add(a);
+                a.getCoordinate().setX(a.getCoordinate().getX()+overlap[0]);
+                a.getCoordinate().setY(a.getCoordinate().getY()+overlap[1]);
             }
         }
         for(Actor c : collisionList){
             launchInteraction(c.getInteraction());
             //Delete actors you collide with
             //TODO: fix this shit
-            ServiceLocator.getActorManager().inactivate(c);
+            //ServiceLocator.getActorManager().inactivate(c);
         }
     }
 
@@ -169,7 +172,7 @@ public class GameWorld {
         return myGameState;
     }
 
-    private boolean overlaps(Actor a1, Actor a2){
+    private int[] overlaps(Actor a1, Actor a2){
         var a1Coordinate = a1.getCoordinate();
         var a1Bounds = a1.getBounds();
         int a1MaxX = a1Coordinate.getX()+a1Bounds.getRelX()+a1Bounds.getWidth();
@@ -184,8 +187,32 @@ public class GameWorld {
         int a2MinY = a2Coordinate.getY()+a2Bounds.getRelY();
 
         boolean xIntersects = (a1MaxX > a2MinX && a1MaxX < a2MaxX) || (a2MaxX > a1MinX && a2MaxX < a1MaxX);
+        int xOverlap = 0;
+        if(a1MaxX > a2MinX){
+            xOverlap = a2MinX - a1MaxX;
+        }
+        else if(a2MaxX > a1MinX){
+            xOverlap = a2MaxX - a1MinX;
+        }
+
+
+        int yOverlap = 0;
         boolean yIntersects = (a1MaxY > a2MinY && a1MaxY < a2MaxY) || (a2MaxY > a1MinY && a2MaxY < a1MaxY);
-        return xIntersects && yIntersects;
+        if(a1MaxY > a2MinY){
+            yOverlap = a2MinY - a1MaxY;
+        }
+        else if(a2MaxY > a1MinY){
+            yOverlap = a2MaxY - a1MinY;
+        }
+
+        int[] overlaps = {xOverlap, yOverlap};
+        if(xIntersects && yIntersects){
+            return overlaps;
+        }
+        else{
+            int[] nullOverlaps = {0,0};
+            return nullOverlaps;
+        }
     }
 
 
